c78aa148e689a25054869eb26f49cf6571beb38b
Lylian BALL <BALL27LY@lycee-ndduroc.com>
2023-09-11 17:22:41 +0200

    random

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,7 @@
                     <p class="mb-0">{{__('You are logged in!')}}</p>
                 </div>
             </div>
+            <img class="img" src="img/image.jpg" alt="roller" width=500px />
         </div>
     </div>
 @stop
